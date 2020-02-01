c09827c4272c4bab07159fd8157f472bc3014d29
Muhammad Idrees <midrees097@gmail.com>
2020-02-01 20:06:53 +0500

    Created the business logic

---
+++ inifinite_list_app/lib/bloc/post_bloc.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:inifinite_list/model/post.dart';
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  final http.Client httpClient;
+
+  PostBloc({@required this.httpClient });
+
+  @override
+  PostState get initialState => PostUninitialized();
+
+  @override
+  Stream<PostState> mapEventToState(
+    PostEvent event,
+  ) async* {
+    final currentState = state;
+    if(event is Fetch && !_hasReachedMax(currentState)) {
+      try{
+        if (currentState is PostUninitialized){
+          final posts = await _fetchPosts(0, 20);
+          yield PostLoaded(posts: posts, hasReachedMax: false);
+          return;
+        }
+        if (currentState is PostLoaded) {
+          final posts = await _fetchPosts(currentState.posts.length, 20);
+          yield posts.isEmpty 
+              ? currentState.copyWith(hasReachedMax: true)
+              : PostLoaded (posts: posts, hasReachedMax: false);
+        }
+      } catch (e) {
+        yield PostError();
+      }
+    }
+  }
+
+  @override
+  Stream<PostState> transformEvents(
+    Stream<PostEvent> events,
+    Stream<PostState> Function(PostEvent event) next,
+  ) {
+    return super.transformEvents(events.debounceTime(Duration(milliseconds: 500)), next);
+  }
+
+  bool _hasReachedMax(PostState state) => state is PostLoaded && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get('https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit');
+
+    if (response.statusCode == 200){
+      final data = json.decode(response.body) as List;
+      
+      return data.map((rawPost){
+        return Post(
+          id: rawPost["id"],
+          title: rawPost["title"],
+          body: rawPost["body"],
+        );
+      }).toList();
+    } else {
+      throw Exception('Error Fetching Posts');
+    }
+  }
+}
